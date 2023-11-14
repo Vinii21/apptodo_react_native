d55dc95e764157510e7e7cafa422538d236e9d68
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-11-13 19:18:18 -0600

    Aun tengo el error

--- server/app.js
+++ server/app.js
@@ -22,9 +22,7 @@ const corsOptions = {
 const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
-// app.use(cors());
 app.use(cors(corsOptions));
-/* app.use(ckeckApiKey); */
 
 app.get("/todos/:id", async (req, res) => {
   const todos = await getTodosByID(req.params.id);
@@ -56,17 +54,10 @@ app.delete("/todos/:id", async (req, res) => {
 
 app.post("/todos/shared_todos", async (req, res) => {
   const { todo_id, user_id, email } = req.body;
-  // const { todo_id, user_id, shared_with_id } = req.body;
   const userToShare = await getUserByEmail(email);
   const sharedTodo = await shareTodo(todo_id, user_id, userToShare.id);
   res.status(201).send(sharedTodo);
 });
-
-// app.get("/todos/:id", async (req, res) => {
-//   const id = req.params.id;
-//   const todo = await getTodo(id);
-//   res.status(200).send(todo);
-// });
 
 app.post("/todos", async (req, res) => {
   const { user_id, title } = req.body;
@@ -77,5 +68,3 @@ app.post("/todos", async (req, res) => {
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
-
-////////////////////////////////////////////////////////////////////
